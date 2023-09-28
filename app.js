2d5ea25699a29eac3e6caff088ca9efaa7cc67cc
klemenslatnar <klemen.slatnar06@gmail.com>
2023-09-28 13:38:14 +0200

    Working database connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
+
+const Product = require("./models/Product");
 
 const app = express();
 
@@ -8,23 +10,33 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-async function run() {
-  try {
-    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.xyukcbr.mongodb.net/${process.env.MONGO_STORE}`;
-    const client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
+// Test route for saving products to db
+// app.get("/save-product", async (req, res) => {
+//   const product = new Product({
+//     name: "Leather Boots",
+//     price: 99.99,
+//     imageUrl:
+//       "https://www.allsaints.com/dw/image/v2/BHHD_PRD/on/demandware.static/-/Sites-allsaints-emea-master-catalog/default/dwec632f9d/images/large/MF561Z/5/MF561Z-5-1.jpg?q=80",
+//   });
+
+//   const savedProduct = await product.save();
+
+//   console.log("Product saved");
+//   res.json({ product: savedProduct });
+// });
+
+try {
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.xyukcbr.mongodb.net/${process.env.MONGO_STORE}`
+    )
+    .then((result) => {
+      console.log("Connection established");
+      app.listen(8000);
+    })
+    .catch((err) => {
+      console.log("Connection failed");
     });
-
-    await client.connect();
-
-    app.listen(8000);
-  } catch (error) {
-    console.log(error);
-  }
+} catch (error) {
+  console.log(error);
 }
-
-run();
